606f0abb88bce29f84bb4517f2ef1dcebcc391e1
Trebossa <48384398+Trebossalol@users.noreply.github.com>
2024-04-12 20:15:22 +0200

    Update CopyLibToPluginsFolder.bat

--- CopyLibToPluginsFolder.bat
+++ CopyLibToPluginsFolder.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set source="C:\Users\Kevin\source\repos\LethalWorkingConditions\LethalWorkingConditions\bin\Debug\LethalWorkingConditions.dll"
+set source="C:\Users\$USERNAME%\source\repos\LethalWorkingConditions\LethalWorkingConditions\bin\Debug\LethalWorkingConditions.dll"
 
 set destination="%appdata%\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\Modding\BepInEx\plugins\LethalWorkingConditions.dll"
 
